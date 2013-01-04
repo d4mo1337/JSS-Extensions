ac39cba54dc3f751786d4ba600f63b257eb86a9f
Damien <d4mo1337@gmail.com>
2013-01-04 15:11:22 +1030

    version 1.1

--- jssJavaPlugin.sh
+++ jssJavaPlugin.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # Extension Attribute checks to display Java Plugin Version number.
-#Version 1.0
+#Version 1.1
 #Edited 4/1/13
 JavaPluginVersion=`/usr/bin/defaults read /Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin/Contents/Info.plist CFBundleVersion`
 echo "<result> $JavaPluginVersion </result>"
