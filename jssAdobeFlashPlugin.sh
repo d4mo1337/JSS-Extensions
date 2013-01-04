d8c69159f478aa4a46bd0583966b2a6f31445d5e
Damien <d4mo1337@gmail.com>
2013-01-04 15:30:04 +1030

    jssAdobeFlashPlugin V 1.0

--- jssAdobeFlashPlugin.sh
+++ jssAdobeFlashPlugin.sh
@@ -3,7 +3,7 @@
 ############################################################################
 #
 # Extension Attribute checks to display Adobe Flash Player Version number.
-# 
+# Version 1 
 #
 ############################################################################
 FlashPluginVersion=`/usr/bin/defaults read /Library/Internet\ Plug-Ins/Flash\ Player.plugin/Contents/Info CFBundleVersion`
